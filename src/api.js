0832181c8f7c7ba864921c95483ada1b0fd9751f
Pushpa-Mali-23 <pushpa.mali@jwero.ai>
2024-10-19 14:33:42 +0530

    remove cookie session id

--- src/api.js
+++ src/api.js
@@ -60,7 +60,7 @@ export function sendSession(data) {
 }
 
 export function sendEndSession(sessionId, sessionEnd = null) {
-  console.log("ending session 2>>>>>>>>>>>>>>>>>>>",sessionId);
+  //console.log("ending session 2>>>>>>>>>>>>>>>>>>>",sessionId);
   const payload = {
     session_id: sessionId,
     session_end: sessionEnd
@@ -87,7 +87,7 @@ export function sendEndSession(sessionId, sessionEnd = null) {
 }
 
 export function sendActivity(activityType,additionalData = {}, typeId = null ) {
-  console.log(WIDGET_ID);
+  //console.log(WIDGET_ID);
   // console.log("hereeeee in send activity");
   // console.log(activityType,"<<activityType");
   // console.log(typeId,"<<typeId");
@@ -106,11 +106,11 @@ export function sendActivity(activityType,additionalData = {}, typeId = null ) {
       //...(typeId ? { type_id: typeId } : {})
     };
 
-    console.log(payload);
-    console.log(typeId ,"<<<<<<<<<<<<<<<<<<<<,updated typeId");
-    console.log("========");
-    console.log(additionalData);
-    console.log("========");
+    //console.log(payload);
+    //console.log(typeId ,"<<<<<<<<<<<<<<<<<<<<,updated typeId");
+    //console.log("========");
+    //console.log(additionalData);
+    //console.log("========");
     // Use navigator.sendBeacon for better performance on unload
     // if (navigator.sendBeacon) {
     //   const blob = new Blob([JSON.stringify(payload)], { type: 'application/json' });
@@ -132,14 +132,14 @@ export function sendActivity(activityType,additionalData = {}, typeId = null ) {
 
 // Utility functions to manage session_id
 export function setSessionId(id) {
-  console.log("settting session id in storage");
+  //console.log("settting session id in storage");
   const storageKey = "session_id";
   setCookie(storageKey, id, 30); // Expires in 30 minutes
   localStorage.setItem(storageKey, id);
 }
 
 function clearSessionId() {
-  console.log("removing session from storage>>>>>>>>>>>>>>>>>>>");
+  //console.log("removing session from storage>>>>>>>>>>>>>>>>>>>");
   const storageKey = "session_id";
   setCookie(storageKey, "", -1); // Expire the cookie
   localStorage.removeItem(storageKey);
@@ -153,18 +153,18 @@ function setCookie(name, value, minutes) {
 }
 
 function getCookie(name) {
-  console.log("============inside getCookie============");
+  //console.log("============inside getCookie============");
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
-  console.log(value,"=========value========");
-  console.log(parts,"=========parts========");
+  //console.log(value,"=========value========");
+  //console.log(parts,"=========parts========");
   if (parts.length === 2)
     return decodeURIComponent(parts.pop().split(";").shift());
 }
 
 export function getSessionId() {
-  console.log("inside getSessionId");
-  console.log(getCookie("session_id"),"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<session id");
+ // console.log("inside getSessionId");
+  //console.log(getCookie("session_id"),"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<session id");
   return getCookie("session_id");
 }
 
